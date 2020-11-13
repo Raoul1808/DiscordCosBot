2431b486e129ed152b94b28e28cd5c881e242cb8
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2020-11-13 19:21:27 +0100

    Chargement automatique des modules

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import discord
 from discord.ext import commands
 import random
+from os import listdir, path, getcwd
 
 # Importation de fichiers locaux
 import config
@@ -10,9 +11,6 @@ import checks
 
 # Objet "bot" qui représente le bot. Essentiel
 bot = commands.Bot(command_prefix=config.botPrefix)
-
-# Chargement des extensions situées dans le dossier "cogs"
-bot.load_extension("cogs.MathsModule")
 
 
 # Évènement : se lance à la connexion à Discord
@@ -30,8 +28,11 @@ async def on_ready():
 # Évènement : se lance quand une erreur non gérée s'est produite dans n'importe quelle partie du code (incluant les fichiers externes et les librairies)
 @bot.event
 async def on_command_error(ctx, error):
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send(":x: On dirait que vous n'avez pas les permissions requises pour exécuter cette commande ...")
+        return
     if not isinstance(error, commands.CommandNotFound):
-        await ctx.send(f"```python\n{error}```")
+        await ctx.send(f":x: Une erreur s'est produite dans le code ...\n```python\n{error}```")
 
 
 # Commande (avec check) : Recharge tous les fichiers de commandes externes
@@ -46,7 +47,36 @@ async def _reload(ctx, cog=None):
         except commands.ExtensionNotFound:
             await ctx.send(":warning: Cette extension n'existe pas")
             return
-    await ctx.send(":x: Je n'ai pas encore la capacité de recharger toutes les extensions")
+    failed = False
+    message = "Rechargement des modules"
+    error = None
+    for cog in listdir(path.join(getcwd(), "cogs")):
+        if cog.endswith('Module.py'):
+            if failed:
+                message += "\n:x: "+cog.replace('py', '')
+                continue
+            try:
+                bot.reload_extension(f"cogs.{cog.replace('py', '')}")
+                message += "\n:white_check_mark: " + cog.replace('py', '')
+            except Exception as e:
+                message += "\n:warning: " + cog.replace('py', '')
+                error = e
+                failed = True
+    await ctx.send(message)
+    if error:
+        raise error
+
+
+# Chargement automatique des modules listés dans le dossier "cogs". Critère de sélection : fichiers finissant par "Module.py"
+for cog in listdir(path.join(getcwd(), "cogs")):
+    if cog.endswith('Module.py'):
+        try:
+            bot.load_extension(f"cogs.{cog.replace('py', '')}")
+            print(f"{cog.replace('py', '')} loaded successfully")
+        except Exception as e:
+            print(f"{cog.replace('py', '')} failed to load")
+            raise e
+
 
 
 # Lance le bot.
