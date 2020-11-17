bf950aae6b580213c541e283c6977cd175726239
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2020-11-17 11:32:47 +0100

    Librairie Random + Anti-Input

--- cogs/MathsModule.py
+++ cogs/MathsModule.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import random
 from math import *
+from random import *
 
 # Création d'un Cog (aka extension de bot)
 class MathsModule(commands.Cog):
@@ -17,12 +18,15 @@ class MathsModule(commands.Cog):
         if expression.find("import ") != -1:
             await ctx.send(":x: N'essayez surtout pas d'importer une librairie ...")
             return
+        if expression.find("input(") != -1:
+            await ctx.send(":x: Je ne peux pas exécuter les méthodes `input()`")
+            return
         try:
             await ctx.send(f"```\n{expression} = {eval(expression)}```")
         except ZeroDivisionError:
             await ctx.send(":warning::warning::warning: **C'EST UN CRIME DE DIVISER PAR 0, EFFACEZ-MOI IMMÉDIATEMENT CE MESSAGE !!!!**")
         except Exception:
-            await ctx.send(":x: J'ai rencontré une erreur durant le calcul, vérifiez qu'il soit bien correctement écrit")
+            await ctx.send(":x: J'ai rencontré une erreur durant le calcul, vérifiez qu'il soit correctement écrit")
 
 
 def setup(bot):
